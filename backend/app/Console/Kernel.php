8b5e6679a311ce132fdd7c82657a99535d149b27
rajbdde <rajcsediu@gmail.com>
2023-05-11 19:56:37 +0200

    recommit

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('insert-news')->hourly();
+        $schedule->command('insert-news')->everyMinute();
     }
 
     /**
